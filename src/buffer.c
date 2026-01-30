8a690797b1ae08fc4b5d6af6a72d90aa8f0fbced
creyes2001 <reyescristopher43@gmail.com>
2026-01-29 20:23:29 -0600

    Fix buffer bugs

--- src/buffer.c
+++ src/buffer.c
@@ -2,41 +2,58 @@
 
 void Buffer_Init(buffer_t *buffer)
 {
-	*(buffer->head) = 0;
-	*(buffer->tail) = 0;
+	buffer->head = 0;
+	buffer->tail = 0;
 	buffer->IsEmpty = false;
 	buffer->IsFull = false;
 }
 
 void Buffer_Add(buffer_t *buffer,uint8_t element)
 {
-	if(*(buffer->head) - *(buffer->tail) == BUFFER_SIZE - 1)
-	{
-		buffer->IsFull = true;
-	}
-
-	if(buffer->IsFull =! true)
-	{
-		if(*(buffer->head) == BUFFER_SIZE)
-		{
-			*(buffer->head) = 0;
-		}
-		RingBuffer[*(buffer->head)] = element;
-		*(buffer->head)++;
-	}
+		if(!buffer->IsFull)
+      {
+          RingBuffer[buffer->head] = element;
+          buffer->head++;
+      }
+      if(buffer->head == BUFFER_SIZE)
+      {
+          buffer->head = 0;
+      }
+  
+      if(buffer->head - buffer->tail >= BUFFER_SIZE)
+      {
+          buffer->IsFull = true;
+      }
+  
+      if(buffer->IsEmpty && (buffer->head != buffer->tail))
+      {
+          buffer->IsEmpty = false;
+      }
 }
 
 uint8_t Buffer_Get(buffer_t *buffer)
 {
-	if(*(buffer->head) == *(buffer->tail))
-	{
-		buffer->IsEmpty = true;
-	}
-	if(buffer->IsEmpty != true)
-	{
-		if(*(buffer->tail) == BUFFER_SIZE)
-		{
-			*(buffer->tail) = 0;
-		}
-	return buffer[*(buffer->tail)];
+	uint8_t tail_temp;
+
+     if(!buffer->IsEmpty)
+     {
+         tail_temp = RingBuffer[buffer->tail];
+         buffer->tail++;
+     }
+
+     if(buffer->tail == BUFFER_SIZE)
+     {
+         buffer->tail = 0;
+     }
+
+     if(buffer->head == buffer->tail)
+     {
+         buffer->IsEmpty = true;
+     }
+
+     if(buffer->IsFull && (buffer->head != buffer->tail))
+     {
+         buffer->IsFull = false;
+     }
+     return tail_temp;
 }
